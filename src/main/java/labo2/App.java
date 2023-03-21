8fc31202903d8c07f52801b31ab76b6ff26420f4
Sam <64200816+samihanine@users.noreply.github.com>
2023-03-20 23:11:21 -0400

    Add style to view

--- src/main/java/labo2/App.java
+++ src/main/java/labo2/App.java
@@ -18,8 +18,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        scene.getStylesheets().add("/labo2/view/style.css"); // import CSS file
+        scene = new Scene(loadFXML("primary"), 800, 600);
+        scene.getStylesheets().add(App.class.getResource("view/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/labo2/controller/ImageController.java
+++ src/main/java/labo2/controller/ImageController.java
@@ -3,14 +3,42 @@ package labo2.controller;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.FileChooser;
-
 import java.io.File;
+import javafx.scene.input.MouseEvent;
 
 public class ImageController {
 
     @FXML
-    private ImageView imageView;
+    private ImageView imageView1;
+
+    @FXML
+    private ImageView imageView2;
+
+    @FXML
+    private ImageView imageView3;
+
+    private double lastX;
+    private double lastY;
+
+    private void initializeImageViews(ImageView... imageViews) {
+        for (ImageView imageView : imageViews) {
+            imageView.setPreserveRatio(true);
+            imageView.setOnScroll(this::handleScroll);
+            imageView.setOnMousePressed(event -> {
+                imageView.setMouseTransparent(true);
+            });
+            imageView.setOnMouseReleased(event -> {
+                imageView.setMouseTransparent(false);
+            });
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        initializeImageViews(imageView2, imageView3);
+    }
 
     public void loadImage() {
         FileChooser fileChooser = new FileChooser();
@@ -18,10 +46,44 @@ public class ImageController {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
         File selectedFile = fileChooser.showOpenDialog(null);
+
+        // Le reste de votre code pour charger l'image
         if (selectedFile != null) {
+            System.out.println("Selected image path: " + selectedFile.toURI().toString()); // Print image path
             Image image = new Image(selectedFile.toURI().toString());
-            imageView.setImage(image);
+            imageView1.setImage(image);
+            imageView2.setImage(image);
+            imageView3.setImage(image);
         }
     }
 
+    private void handleScroll(ScrollEvent event) {
+        ImageView imageView = (ImageView) event.getSource();
+        double zoomFactor = 1.05;
+        double deltaY = event.getDeltaY();
+        if (deltaY < 0) {
+            zoomFactor = 0.95;
+        }
+        imageView.setScaleX(imageView.getScaleX() * zoomFactor);
+        imageView.setScaleY(imageView.getScaleY() * zoomFactor);
+        event.consume();
+    }
+
+    private void handleMousePressed(MouseEvent event) {
+        ImageView imageView = (ImageView) event.getSource();
+        lastX = event.getSceneX();
+        lastY = event.getSceneY();
+    }
+
+    private void handleMouseDragged(MouseEvent event) {
+        ImageView imageView = (ImageView) event.getSource();
+        double deltaX = event.getSceneX() - lastX;
+        double deltaY = event.getSceneY() - lastY;
+
+        imageView.setTranslateX(imageView.getTranslateX() + deltaX);
+        imageView.setTranslateY(imageView.getTranslateY() + deltaY);
+
+        lastX = event.getSceneX();
+        lastY = event.getSceneY();
+    }
 }
