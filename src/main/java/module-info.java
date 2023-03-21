04e28b1b2517636edd55d4be8e93070df06d8aca
Sam <64200816+samihanine@users.noreply.github.com>
2023-03-21 04:19:38 -0400

    Delete target directory

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module labo2 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens labo2.view to javafx.fxml;
     opens labo2.controller to javafx.fxml;
 
     exports labo2.controller;
